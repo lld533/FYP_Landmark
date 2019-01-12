4d347fbb765c1af70254edbdfb9e4d2f65ff5b69
Lida Li <lld533@hotmail.com>
2019-01-12 21:54:04 +0800

    Update desktop_model.py
    1/ Implement coords2heatmaps (test code included)
    2/ Rename heatmap2coords to heatmaps2coords

--- models/desktop_model.py
+++ models/desktop_model.py
@@ -88,13 +88,67 @@ def argmax_2d(tensor):
     # concat and return coordinates
     return tf.concat([argmax_x, argmax_y], axis=-1)
 
-def heatmap2coords(heatmaps):
+def heatmaps2coords(heatmaps):
     # Input:
     # heatmap: [N, H, W, K], N: batch size, H: height, W: width, K: #keypoints
     #
     # Output
     # coords: [N, 2K], N: batch size, K: #keypoints organized by x_1..x_Ky_1..y_K
     return tf.cast(argmax_2d(heatmaps), dtype=tf.float32)
+
+def coords2heatmaps(coords, shape, value=1.0):
+    # Input:
+    # coords: [N, 2K], N: batch size, K: #keypoints organized by x_1..x_Ky_1..y_K
+    # shape: a list of 2 elements, (H, W), where H stands for height, W for width.
+    # value: fill the value at the coord in the corresponding heatmap (default 1.0).
+    #
+    # Output
+    # heatmap: [N, H, W, K], N: batch size, H: height, W: width, K: #keypoints
+    
+    constant_val = tf.convert_to_tensor([value], 
+                                      dtype=tf.float32)
+    def coords2heatmaps_image(coord):        
+        def coords2heatmaps_channel(coord):                        
+            is_x_valid = tf.logical_and(tf.greater_equal(coord[0], 
+                                                         0.0), 
+                                        tf.less_equal(coord[0], 
+                                                      float(int(shape[1])-1))
+                                        )
+            is_y_valid = tf.logical_and(tf.greater_equal(coord[1], 
+                                                         0.0), 
+                                        tf.less_equal(coord[1], 
+                                                      float(int(shape[0])-1))
+                                        )
+            is_coord_valid = tf.logical_and(is_x_valid, is_y_valid)
+            
+            def set_coord(coord):
+                coord = tf.cast(tf.round(coord), dtype=tf.int64)
+                coord = coord[::-1] #convert x-y to y-x
+                coord = tf.expand_dims(coord, 0)
+                return tf.sparse.to_dense(tf.sparse.SparseTensor(coord, 
+                                                                 constant_val, 
+                                                                 shape))
+            
+            def skip_coord():
+                return tf.zeros(shape, dtype=tf.float32)
+            
+            # If the coord can be plotted on the heatmap, plot it!
+            # Otherwise, skip it.
+            heatmap = tf.cond(is_coord_valid, 
+                              lambda: set_coord(coord), 
+                              skip_coord)
+            
+            return heatmap
+        
+        coord = tf.reshape(coord, [2,-1])#[2,K]
+        coord = tf.transpose(coord, perm=[1,0])#[K,2]
+        
+        return tf.map_fn(lambda x: coords2heatmaps_channel(x),
+                         coord)
+    
+    return tf.map_fn(lambda x : coords2heatmaps_image(x), 
+                     coords)
+    
 
 """
 Test code
@@ -103,10 +157,10 @@ Test code
 #imgs = tf.placeholder(tf.float32, [None, 96,96, 3])
 #prediction_heatmap = model_desktop_gap(imgs)
 
-## function heatmap2coords
+## function heatmaps2coords
 #np_heatmap = np.random.rand(1,96,96,68)
 #pseudo_heatmap = tf.convert_to_tensor(np_heatmap, dtype=tf.float32)
-#prediction_coords = heatmap2coords(pseudo_heatmap)
+#prediction_coords = heatmaps2coords(pseudo_heatmap)
 
 #print("="*32)
 #print("np_heatmap:")
@@ -116,3 +170,13 @@ Test code
 
 #with tf.Session() as sess:
     #print(sess.run(prediction_coords))
+
+## function coords2heatmaps    
+#import numpy as np
+#mat = np.random.rand(32, 68*2) * 40.0
+#mat = mat.astype(np.float32)
+#heatmaps = coords2heatmaps(tf.convert_to_tensor(mat), 
+                           #(40,40))
+
+#with tf.Session() as sess:
+    #print(sess.run(heatmaps))
